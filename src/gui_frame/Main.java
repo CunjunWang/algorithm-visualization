62bbc9468294de89edffe03b3b2336b8181b99e0
Cunjun Wang <duckwcj@gmail.com>
2019-12-15 10:36:19 -0500

    initial commit

---
+++ src/gui_frame/Main.java
@@ -0,0 +1,17 @@
+package gui_frame;
+
+import javax.swing.*;
+
+/**
+ * Created by CunjunWang on 2019-12-15.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Welcome");
+        frame.setSize(500,500);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
